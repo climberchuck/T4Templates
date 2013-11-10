a9033e24045d565aa27c522857e383080845263e
climberchuck <chuck.bridgen@gmail.com>
2013-11-10 17:49:57 -0500

    Initial Commit

---
+++ T4Templates.Entities/MyDataContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T4Templates.Entities {
+    public class MyDataContext:DbContext {
+        public DbSet<Person> People { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Vendor> Vendors { get; set; }
+    }
+    public class Person {
+        public Guid PersonKey { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+    public class Project {
+        public Guid ProjectKey { get; set; }
+        public Guid Name { get; set; }
+        public IEnumerable<Vendor> Vendors { get; set; }
+    }
+    public class Vendor {
+        public Guid VendorKey { get; set; }
+        public Guid Name { get; set; }
+    }
+}
---
+++ T4Templates.Examples/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using T4Templates.Entities;
+
+namespace T4Templates.Examples {
+    class Class1 {
+        private MyDataContext db = new MyDataContext();
+    }
+}
